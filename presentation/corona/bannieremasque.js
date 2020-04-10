e2ef42f8706f5a234ee0d8b86f01fe06e1c7c1dd
Thierry Perrotin <tp@t-p.tf>
2020-04-10 18:24:13 +0200

    add bannière masque

--- presentation/corona/bannieremasque.js
+++ presentation/corona/bannieremasque.js
@@ -1,3 +1,9 @@
-	$(document).ready(function () {      
-				$("#banniereMasqueCOVID").load("https://thierryperrotin.github.io/cci-dev-design/presentation/corona/bannieremasque.html");
+	$(document).ready(function () {  
+		TailleMax = $("#banniereMasqueCOVID").attr('Taille-Max');
+		Lien = $("#banniereMasqueCOVID").attr('Lien');
+				$("#banniereMasqueCOVID").load("https://thierryperrotin.github.io/cci-dev-design/presentation/corona/bannieremasque.html",
+				function(){
+					$("#MasqueCOVID").css("max-width", TailleMax);
+					$("#MasqueCOVID").attr("href", Lien);
+				});
 	});
